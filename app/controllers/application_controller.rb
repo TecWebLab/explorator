315bcb93194b0707f3a83640643234314ba67382
samuraraujo <samuraraujo@77a1f9a2-eb6d-eb40-af56-193475806454>
2008-10-20 15:32:30 +0000

    git-svn-id: http://www.tecweb.inf.puc-rio.br/svn/explorator/Explorator/trunk@113 77a1f9a2-eb6d-eb40-af56-193475806454

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 #Author: Samur Araujo
 #Date: 25 jun 2008.
 class CssController < ApplicationController
+   session :disabled => false
+
   #Default rails method for a controller.
   def index    
   end
