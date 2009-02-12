9e64882ae24bee176ce2a7ae35b68c072c9490f0
samuraraujo <samuraraujo@77a1f9a2-eb6d-eb40-af56-193475806454>
2009-02-12 21:17:29 +0000

    git-svn-id: http://www.tecweb.inf.puc-rio.br/svn/explorator/Explorator/trunk@301 77a1f9a2-eb6d-eb40-af56-193475806454

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-#This Controller just is used to supply the css's user interface 
-#Author: Samur Araujo
-#Date: 25 jun 2008.
-class CssController < ApplicationController
-   
-  #Default rails method for a controller.
-  def index    
-  end
-end
