56f221fc68e458bfdba531d6876fa887912ddd3d
samuraraujo <samuraraujo@77a1f9a2-eb6d-eb40-af56-193475806454>
2009-02-13 19:12:56 +0000

    git-svn-id: http://www.tecweb.inf.puc-rio.br/svn/explorator/Explorator/trunk@307 77a1f9a2-eb6d-eb40-af56-193475806454

--- lib/libs.rb
+++ lib/libs.rb
@@ -10,5 +10,8 @@ require 'explorator_application.rb'
 #adapter = ConnectionPool.add(:type => :rdflite, :location => 'db/explorator.db', :reasoning => false,:keyword => true)
 $work = ConnectionPool.add_data_source :type => :sparql_sesame_api ,   :repository => 'WORK', :dir => $sesamedir.path
 $work.title='WORK'
+
+#maximum number of facets display on the interface
+$VIEW_FACET_NUMBER=10
  
 
