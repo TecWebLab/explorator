3b1e6e99530d979715fc8aab10c1e18ad69a6728
samuraraujo <samuraraujo@77a1f9a2-eb6d-eb40-af56-193475806454>
2009-04-09 00:01:55 +0000

    git-svn-id: http://www.tecweb.inf.puc-rio.br/svn/explorator/Explorator/trunk@395 77a1f9a2-eb6d-eb40-af56-193475806454

---
+++ lib/memory_store.rb
@@ -0,0 +1,32 @@
+begin 
+      class MemoryStore < ActionController::Session::AbstractStore
+        def initialize(app, options = {})
+          # Support old :expires option
+          options[:expire_after] ||= options[:expires]
+          
+          super        
+          
+          @pool = Hash.new
+          @mutex = Mutex.new
+          
+          super
+        end
+        
+        private
+        def get_session(env, sid)
+          sid ||= generate_sid  
+            session = @pool[sid] || {}
+           [sid, session]
+        end
+        
+        def set_session(env, sid, session_data)
+          options = env['rack.session.options']
+          expiry  = options[:expire_after] || 0
+          @pool[sid]=session_data
+          return true         
+        end
+      end
+ 
+rescue LoadError
+  # MemCache wasn't available so neither can the store be
+end
