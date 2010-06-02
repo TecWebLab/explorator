40ef5b9c4ce531a8870dec02a8308ab52af0102a
samuraraujo <samuraraujo@77a1f9a2-eb6d-eb40-af56-193475806454>
2010-06-02 21:38:22 +0000

    git-svn-id: http://www.tecweb.inf.puc-rio.br/svn/explorator/Explorator/trunk@478 77a1f9a2-eb6d-eb40-af56-193475806454

--- lib/memory_store.rb
+++ lib/memory_store.rb
@@ -13,15 +13,26 @@ begin
         end
         
         private
+         def load_session(env)
+          request = Rack::Request.new(env)
+           
+          sid = request.cookies[@key]
+           
+          sid = request.params['session_id'] if sid == nil
+          
+          unless @cookie_only
+            sid ||= request.params[@key]
+          end
+          sid, session = get_session(env, sid)
+          [sid, session]
+        end
         def get_session(env, sid)
           sid ||= generate_sid  
-            session = @pool[sid] || {}
-           
+            session = @pool[sid] || {}           
            [sid, session]
         end
         
-        def set_session(env, sid, session_data)
-          
+        def set_session(env, sid, session_data)          
           options = env['rack.session.options']
           expiry  = options[:expire_after] || 0
           @pool[sid]=session_data
